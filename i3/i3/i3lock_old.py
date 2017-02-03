a7b232595419b144090e96a07df5afe27d1f709b
Shobhit Sharma <shobhit@coverfoxmail.com>
2017-02-03 17:49:15 +0530

    Updated config

--- i3/i3/i3lock_old.py
+++
@@ -1,9 +0,0 @@
-import random
-from os import listdir
-from os.path import isfile, join
-from subprocess import call
-
-mypath = "/home/shobhit/lock_screen/png"
-onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
-img = onlyfiles[random.randint(0, len(onlyfiles))]
-call(["i3lock", "-i", "{0}/{1}".format(mypath, img)])
